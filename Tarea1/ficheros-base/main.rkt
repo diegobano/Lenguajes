d85838e29befa8f6c3eefc83442f3c9927c0328e
diegobano <diego.r.bano@hotmail.com>
2017-05-04 22:29:08 -0300

    Added some comments

--- Tarea1/ficheros-base/main.rkt
+++ Tarea1/ficheros-base/main.rkt
@@ -29,13 +29,16 @@
   (TNum)
   (TFun arg ret))
 
-; parse-type : Expr -> Type
+; parse-type : <type> -> Type
+; Transforma una lista de elementos de la forma {Num -> Num} o Num en un Type
 (define (parse-type s-expr)
   (match s-expr
     ['Num (TNum)]
     [(list arg -> ret) (TFun (parse-type arg) (parse-type ret))]
     [else (error "Parse error")])) 
 
+; parse : expr -> Expr
+; Transforma una lista de elementos en una Expr
 (define (parse s-expr)
   (match s-expr
     [(? number?) (num s-expr)]
@@ -50,40 +53,82 @@
 
 (define (compile expr) #f)
 
+; typeof : Expr -> Type
+; Recibe una Expr y retorna sus tipos en formato Type
+; Para calcular los tipos ocupa la funcion auxiliar typeof-env ocupando ambientes para definir variables
 (define (typeof expr)
+  (typeof-env expr empty-env))
+
+; typeof-env : Expr Env -> Type
+; Recibe una Expr y el ambiente (Env) en la que esta definida y calcula sus tipos
+(define (typeof-env expr env)
   (match expr
     [(num n) (TNum)]
-    [(id x) (TNum)]
+    [(id x) (env-lookup x env)]
     [(add l r) (if (not (equal? (TNum) (typeof l)))
-                   (error (string-append "Type error in expression + position 1: expected Num found " (parse-msg (typeof l))))
+                   (error (string-append "Type error in expression + position 1: expected Num found " (symbol-to-str (parse-msg (typeof l)))))
                    (if (not (equal? (TNum) (typeof r)))
-                       (error (string-append "Type error in expression + position 2: expected Num found " (parse-msg (typeof r))))
+                       (error (string-append "Type error in expression + position 2: expected Num found " (symbol-to-str (parse-msg (typeof r)))))
                        (TNum)))]
     [(fun id targ body tbody) (if (not tbody)
-                                  (TFun (TNum) (typeof body))
-                                  (let ((etype (typeof body)))
+                                  (TFun (TNum) (typeof-env body (env-extend id targ env)))
+                                  (let ((etype (typeof-env body (env-extend id targ env))))
                                     (if (equal? tbody etype)
                                         (TFun (TNum) (TNum))
-                                        (let ((emsg1 (parse-msg etype)) (emsg2 (parse-msg tbody)))
+                                        (let ((emsg1 (symbol-to-str (parse-msg etype))) (emsg2 (symbol-to-str (parse-msg tbody))))
                                           (error (string-append "Type error in expression fun position 1: expected " emsg2 " found " emsg1))))))]
                                   
-    [(app fun-id arg-expr) (let ((fun-types (typeof fun-id)))
+    [(app fun-id arg-expr) (let ((fun-types (typeof-env fun-id env)))
                              (if (equal? fun-types TNum)
-                                 (let ((emsg (parse-msg fun-types)))
+                                 (let ((emsg (symbol-to-str (parse-msg fun-types))))
                                    (error (string-append "Type error in expression app position 1: expected {Num -> Num} found " emsg)))
-                                 (let ((targ (typeof arg-expr)))
+                                 (let ((targ (typeof-env arg-expr env)))
                                    (if (not (equal? (TFun-arg fun-types) targ))
-                                       (let ((emsg1 (parse-msg (TFun-arg fun-types))) (emsg2 (parse-msg targ)))
+                                       (let ((emsg1 (symbol-to-str (parse-msg (TFun-arg fun-types)))) (emsg2 (symbol-to-str (parse-msg targ))))
                                           (error (string-append "Type error in expression app position 2: expected " emsg1 " found " emsg2)))
                                        (TFun-ret fun-types)))))]))
 
-
+; typecheck : <s-expr> -> <type>
+; Recibe una lista de simbolos de la forma <s-expr> y la retorna de la forma <type>
 (define (typecheck s-expr)
-  (string->symbol (parse-msg (typeof (parse s-expr)))))
+  (parse-msg (typeof (parse s-expr))))
 
+; parse-msg : Type -> <type>
 (define (parse-msg type)
   (match type
-    [(TNum) (symbol->string 'Num)]
-    [(TFun arg ret) (string-append "{" (parse-msg arg) " -> " (parse-msg ret) "}")]))
+    [(TNum) 'Num]
+    [(TFun arg ret) (list (parse-msg arg) '-> (parse-msg ret))]))
+
+(define (symbol-to-str msg)
+  (match msg
+    [(? symbol?) (symbol->string msg)]
+    [(list arg '-> ret) (string-append "{" (symbol-to-str arg) " -> " (symbol-to-str ret) "}")]))
 
 (define (typed-compile s-expr) #f)
+
+#|-----------------------------
+Environment abstract data type
+ 
+empty-env  :: Env
+env-extend :: Sym Val Env -> Env
+env-lookup :: Sym Env -> Val
+ 
+representation BNF:
+<env> ::= (mtEnv)
+        | (aEnv <id> <val> <env>)
+|#
+(deftype Env
+  (mtEnv)
+  (aEnv id type env))
+ 
+(def empty-env  (mtEnv))
+ 
+(def env-extend aEnv)
+ 
+(define (env-lookup x env)
+  (match env 
+    [(mtEnv) (error 'env-lookup "free identifier: ~a" x)]
+    [(aEnv id val rest)
+     (if (equal? id x)
+         val
+         (env-lookup x rest))]))
